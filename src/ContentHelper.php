9a17b6a0f331abed337f66bf20e06c6d0bbba6c6
Spencer Penrod <penrodspencer@gmail.com>
2022-02-08 17:06:02 -0500

    Added more functions to ContentHelper, and have started working on first test method

--- src/ContentHelper.php
+++ src/ContentHelper.php
@@ -4,14 +4,38 @@ declare(strict_types=1);
 
 class ContentHelper
 {
+
+    protected string $contents;
+    protected DOMDocument $dom;
+
+    public function __construct()
+    {
+        $contents = file_get_contents(path_join(plugin_dir_path(__FILE__), 'arngren_net.html'));
+        $dom = new DOMDocument();
+        $dom->loadHTML($contents);
+    }
+
+    /*
+        Given a html string, either an entire or partial document, return all image elements (these can be DOMElement instances)
+    */
     public function get_images(string $html): DOMNOdeList
     {
-        $dom = new DOMDocument();
         $dom->loadHTML($html);
         $images = $dom->getElementsByTagName('img');
         return $images;
     }
 
+    /*
+        Given a DOMElement node, return either null or its caption
+    */
+    public function get_image_caption(DOMElement $element): ?string
+    {
+        return '';
+    }
+
+    /*
+        Given a DOMElement node, return either null or its alt
+    */
     public function get_image_alt(DOMElement $element): ?string
     {
         $attr = $element->getAttribute('alt');
@@ -21,6 +45,9 @@ class ContentHelper
         return $attr;
     }
 
+    /*
+        Given an image DOMElement node and a string, set its alt attribute value. Be careful of data sanitation.
+    */
     public function set_image_alt(DOMElement $element, string $alt): DOMElement
     {
         $attr=$element->getAttribute('alt');
@@ -28,6 +55,9 @@ class ContentHelper
         return $newelement;
     }
 
+    /*
+        Given a DOMElement node, return either null or its src attribute
+    */
     public function get_image_src(DOMElement $element): ?string
     {
         $src = $element->getAttribute('src');
@@ -46,14 +76,58 @@ class ContentHelper
         return $src;
     }
 
+    /*
+        Given a DOMElement node, return either null or its class attribute
+    */
+    public function get_image_class(DOMElement $element): ?string
+    {
+        $src = $element->getAttribute('class');
+        if($src = ''){
+            return null;
+        }
+        return $src;
+    }
+
+    /*
+        Given a DOMElement node, return either null or its attachment src
+    */
+    public function get_image_attachment_src(DOMElement $element): ?string
+    {
+        return null;
+    }
+
+    /*
+        Given a html string, either an entire or partial document, 
+        and a map of image sources and their alt attributes, 
+        set the alt text for each image and return the modified html
+    */
     public function set_image_alts(string $html, $map): string
     {
+        
         $images = get_images($html);
-        $element; 
-        foreach ($images as &$image) {
-            $element = $dom->createElement('img', $image);
+        $image_count = count($images);
 
+        foreach($images as $image){
+            if($map->get($this->get_image_src($image) !== null)){
+                
+            }
         }
+        
+        
+        
     }
+
+
+
+
+    /**
+     * @param int $attachment_id
+     * @return string
+     */
+    function coyote_attachment_url($attachment_id) {
+
+        return '//teststring';
+    }
+
 
 }
--- tests/TestContentHelper.php
+++ tests/TestContentHelper.php
@@ -1,72 +1,66 @@
 <?php
 
 declare(strict_types=1);
-
 use PHPUnit\Framework\TestCase;
 
 class TestContentHelper extends TestCase
 {
 
-    protected string $contents;
-    protected DOMDocument $dom;
 
-    public function __construct(string $file)
+    /*
+        Given an html string we can get all of the image elements
+    */
+    public function testGetImages(): void
     {
         $contents = file_get_contents(path_join(plugin_dir_path(__FILE__), 'arngren_net.html'));
         $doc = new DOMDocument();
+    
         $doc->loadHTML($contents);
+        $images = $doc->getElementsByTagName('img');
+        $num = count($images);
+        $this->assertNumeric($num);
     }
 
-    public function test_get_images(string $html): void
+    public function testGetImageAlt(): void
     {
-        $dom = new DOMDocument();
-        $dom->loadHTML($html);
-        $images = $dom->getElementsByTagName('img');
-        return $images;
-    }
-
-    public function test_get_image_alt(DOMElement $element): void
-    {
+        $contents = file_get_contents(path_join(plugin_dir_path(__FILE__), 'arngren_net.html'));
+        $doc = new DOMDocument();
         $attr = $element->getAttribute('alt');
         if($attr = ''){
-            return null;
+            $this->assertNull($attr);
         }
-        return $attr;
+
     }
 
-    public function test_set_image_alt(DOMElement $element, string $alt): void
+    public function testSetImageAlt(): void
     {
         $attr=$element->getAttribute('alt');
         $newelement = $element->setAttribute('alt',$alt);
-        return $newelement;
     }
 
-    public function test_get_image_src(DOMElement $element): void
+    public function testGetImageSrc(): void
     {
         $src = $element->getAttribute('src');
         if($src = ''){
-            return null;
+ 
         }
-        return $src;
     }
 
-    public function test_set_image_src(DOMElement $element): void
+    public function testSetImageSrc(): void
     {
         $src = $element->getAttribute('src');
         if($src = ''){
-            return null;
         }
-        return $src;
     }
 
-    public function test_set_image_alts(string $html, $map): string
+    public function testGetImageClass(): void
     {
-        $images = get_images($html);
-        $element; 
-        foreach ($images as &$image) {
-            $element = $dom->createElement('img', $image);
-
+        $class = $element->getAttribute('class');
+        if($src = ''){
+            $this->assertNull($class);
         }
+        $this->assertSame();
     }
+
 
 }
