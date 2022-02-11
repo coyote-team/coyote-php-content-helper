524e28dce21565ab097b23c51bb41bc491ddd5dc
Spencer Penrod <penrodsw@miamioh.edu>
2022-02-11 14:56:56 -0500

    Implemented changing multiple alt attribute in elements with same source

--- tests/TestContentHelper.php
+++ tests/TestContentHelper.php
@@ -45,8 +45,6 @@ class TestContentHelper extends TestCase
         $this->assertInstanceOf(ContentHelper\Image::class, array_pop($images));
     }
 
-
-
     public function testSetAltWithMap(){
         $helper = new ContentHelper("<img src='foo.jpg'><img src='boo.jpg'><img src='notthere.jpg'>");
         $map = ['foo.jpg' => 'one', 'boo.jpg' => 'two', 'fail.jpg' => 'shouldnt work'];
@@ -92,6 +90,24 @@ class TestContentHelper extends TestCase
 
         $this->assertEquals($alt1, 'one');
         $this->assertEquals($alt2, 'two');
+        $this->assertEquals($alt3, 'five');
+    }
+
+    public function testChangeElementsWithSameSrc(){
+        $helper = new ContentHelper("<img src='foo.jpg' alt='three'><img src='foo.jpg' alt='four'><img src='notthere.jpg' alt='five'>");
+        $map = ['foo.jpg' => 'one', 'boo.jpg' => 'two', 'fail.jpg' => 'shouldnt work'];
+        
+        $newText = $helper->set_image_alts($map);
+        $newHelper = new ContentHelper($newText);
+        
+        $images = $newHelper->get_images();
+        
+        $alt1 = $images[0]->alt;
+        $alt2 = $images[1]->alt;
+        $alt3 = $images[2]->alt;
+
+        $this->assertEquals($alt1, 'one');
+        $this->assertEquals($alt2, 'one');
         $this->assertEquals($alt3, 'five');
     }
 
