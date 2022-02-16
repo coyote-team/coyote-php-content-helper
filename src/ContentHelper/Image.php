bcd5336c0e83601ec12825b9042e7e5aba72093c
Spencer Penrod <penrodspencer@gmail.com>
2022-02-16 13:04:22 -0500

    Removed caption property

--- src/ContentHelper/Image.php
+++ src/ContentHelper/Image.php
@@ -5,14 +5,12 @@ namespace ContentHelper;
 class Image {
     public readonly string $src;
     public readonly ?string $alt;
-    public readonly ?string $caption;
     public readonly ?string $class;
 
-    public function __construct(string $src, string $alt, string $caption, string $class)
+    public function __construct(string $src, string $alt, string $class)
     {
         $this->src = $src;
         $this->alt = $alt;
-        $this->caption = $caption;
         $this->class = $class;
     }
 }
