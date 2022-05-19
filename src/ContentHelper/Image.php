2b1178ba2604de084a1834e61d425c6e19591f47
Job van Achterberg <mail@3ode.nl>
2022-05-19 09:06:21 +0200

    Support nullable figureCaption property on Image

--- src/ContentHelper/Image.php
+++ src/ContentHelper/Image.php
@@ -9,21 +9,23 @@ class Image
     private ?string $class;
     private ?string $content_before;
     private ?string $content_after;
+    private ?string $figureCaption;
 
     public function __construct(
         string $src,
         string $alt,
         string $class,
         string $content_before = '',
-        string $content_after = ''
+        string $content_after = '',
+        string $figureCaption = null
     ) {
         $this->src = $src;
         $this->alt = $alt;
         $this->class = $class;
         $this->content_before = $content_before;
         $this->content_after = $content_after;
+        $this->figureCaption = $figureCaption;
     }
-
 
     /**
      * @return string
@@ -63,5 +65,10 @@ class Image
     public function getContentAfter(): ?string
     {
         return $this->content_after;
+    }
+
+    public function getFigureCaption(): ?string
+    {
+        return $this->figureCaption;
     }
 }
