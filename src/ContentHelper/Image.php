a51a5ca45c6701540d4941a22f631ead8e477f5f
Job van Achterberg <mail@3ode.nl>
2022-05-19 09:08:44 +0200

    Use consistent variable casing style

--- src/ContentHelper/Image.php
+++ src/ContentHelper/Image.php
@@ -7,23 +7,23 @@ class Image
     private string $src;
     private ?string $alt;
     private ?string $class;
-    private ?string $content_before;
-    private ?string $content_after;
+    private ?string $contentBefore;
+    private ?string $contentAfter;
     private ?string $figureCaption;
 
     public function __construct(
         string $src,
         string $alt,
         string $class,
-        string $content_before = '',
-        string $content_after = '',
+        string $contentBefore = '',
+        string $contentAfter = '',
         string $figureCaption = null
     ) {
         $this->src = $src;
         $this->alt = $alt;
         $this->class = $class;
-        $this->content_before = $content_before;
-        $this->content_after = $content_after;
+        $this->contentBefore = $contentBefore;
+        $this->contentAfter = $contentAfter;
         $this->figureCaption = $figureCaption;
     }
 
@@ -44,12 +44,12 @@ class Image
 
     public function getContentBefore(): ?string
     {
-        return $this->content_before;
+        return $this->contentBefore;
     }
 
     public function getContentAfter(): ?string
     {
-        return $this->content_after;
+        return $this->contentAfter;
     }
 
     public function getFigureCaption(): ?string
